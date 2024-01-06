d4ec5023ae3f808c92a075675224da800a7704e4
Wessley Silva <wessley.silva@outlook.com.br>
2024-01-05 21:38:52 -0300

    atualização de icone ao canto inferior direito, de help

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ elementosDuvida.forEach(function(duvida) {
     })
 })
 
+var whatsappHelp = document.getElementById('whatsapp')
+
+whatsappHelp.addEventListener('mouseover', crescerBorda)
+
+whatsappHelp.addEventListener('mouseout', removeBorda)
+
+
+function crescerBorda() {
+    var icone = document.getElementById('whatsapp')
+    icone.style.outline = `10px solid #ff9a6c`;
+    icone.style.transition= '0.5s ease-in-out'
+}
+
+function removeBorda() {
+    var icone = document.getElementById('whatsapp')
+
+    icone.style.outline = '0px solid #ff9a6c';
+
+    
+
+}
+
